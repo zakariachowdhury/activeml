03c7dc93ebb36313b14385929d208604f61e48a1
Zakaria Chowdhury <zakaria.cse@gmail.com>
2020-10-25 00:00:45 -0500

    Add group option in the sidebar to display individual analysis

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,22 @@ import io
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-APP_TITLE = "EDA"
-APP_SUBHEADER = "Exploratory Data Analysis"
+APP_TITLE = "Exploratory Data Analysis"
 SAMPLE_DATA_URL = 'https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv'
+
+GROUP_BASIC = 'Basic Analysis'
+GROUP_CATEGORICAL = 'Categorical Analysis'
+GROUP_NUMERICAL = 'Numerical Analysis'
+GROUP_BIVARIATE = 'Bivariate Analysis'
+GROUP_MULTIVARIATE = 'Multivariate Analysis'
+
+groups = [
+    GROUP_BASIC,
+    GROUP_CATEGORICAL,
+    GROUP_NUMERICAL,
+    GROUP_BIVARIATE,
+    GROUP_MULTIVARIATE
+]
 
 @st.cache
 def load_data(url, nrows=None, sep=','):
@@ -20,8 +33,12 @@ def load_data(url, nrows=None, sep=','):
 def st_plot(plot):
     st.pyplot(plot.get_figure(), clear_figure=True)
 
+def section_title(text):
+    st.markdown(f'*{text}*:')
+
 def main():
     df = None
+    selected_groups = []
     categorical_columns = None
     nummeric_columns = None
 
@@ -32,7 +49,7 @@ def main():
         initial_sidebar_state="auto")
 
     st.title(APP_TITLE)
-    st.subheader(APP_SUBHEADER)
+    st.text('Enter your dataset csv url below to get started.')
 
     with st.beta_expander("Data", True):
         col1, col2 = st.beta_columns(2)
@@ -54,9 +71,11 @@ def main():
             columns = st.multiselect('Columns:', list(df.columns), list(df.columns))
             df = df[columns]
             
-            categorical_columns = df.select_dtypes(include=['object']).columns.tolist()
-            nummeric_columns = df.select_dtypes(include=np.number).columns.tolist()
+            cat_col = df.select_dtypes(include=['object']).columns.tolist()
+            num_col = df.select_dtypes(include=np.number).columns.tolist()
 
+            categorical_columns = st.multiselect('Categorical Columns:', columns, cat_col)
+            nummeric_columns = st.multiselect('Numerical Columns:', num_col, num_col)
             date_columns = st.multiselect('Date Columns:', categorical_columns)
             for col in date_columns:
                 try:
@@ -64,84 +83,105 @@ def main():
                 except Exception as e:
                     st.error(f'Column \'{col}\' cannot be converted to datetime')
 
-            categorical_columns = df.select_dtypes(include=['object']).columns.tolist()
-
-            '*Shape:*'
+            section_title('Shape')
             df.shape
 
-            '*Head:*'
+            section_title('Head')
             df[:5]
 
-            '*Tail:*'
-            df[-5:]
+            if st.checkbox("View All"):
+                df
     
     if df is not None and len(df):
-        with st.beta_expander("Basic Analysis"):
-            '*Info:*'
-            buffer = io.StringIO()
-            df.info(buf=buffer)
-            st.text(buffer.getvalue())
+        with st.sidebar.beta_expander("Groups", True):
+            for group in groups:
+                if st.checkbox(group):
+                    selected_groups.append(group)
 
-            '*Data Types:*'
-            dtypes = df.dtypes.rename('Total')
-            dtypes
+        if GROUP_BASIC in selected_groups:
+            with st.beta_expander(GROUP_BASIC, True):
+                section_title('Info')
+                buffer = io.StringIO()
+                df.info(buf=buffer)
+                st.text(buffer.getvalue())
 
-            if len(df.columns):
-                '*Describe:*'
-                desc = df.describe()
-                desc
+                section_title('Data Types')
+                dtypes = df.dtypes.rename('Total')
+                dtypes
 
-            '*Missing Values:*'
-            isnull = df.isnull().sum().rename('Total')
-            isnull
+                if len(df.columns):
+                    section_title('Describe')
+                    desc = df.describe().T
+                    desc
 
-            '*Skew:*'
-            skew = df.skew()
-            skew
-            
-            '*Covariance:*'
-            cov = df.cov()
-            cov
-            if len(cov):
-                st_plot(sns.heatmap(cov, annot=True))
+                section_title('Missing Values')
+                isnull = df.isnull().sum().rename('Total')
+                isnull
 
-            '*Correlation:*'
-            corr = df.corr()
-            corr
-            if len(corr):
-                st_plot(sns.heatmap(corr, annot=True))
+                section_title('Skew')
+                skew = df.skew()
+                skew
+                
+                section_title('Covariance')
+                cov = df.cov()
+                cov
+                if len(cov):
+                    st_plot(sns.heatmap(cov, annot=True))
+
+                section_title('Correlation')
+                corr = df.corr()
+                corr
+                if len(corr):
+                    st_plot(sns.heatmap(corr, annot=True))
         
-        with st.beta_expander('Categorical Columns:'):
-            categorical_columns
-            for col in categorical_columns:
-                count = df[col].value_counts()
-                count
+        if GROUP_CATEGORICAL in selected_groups:
+            with st.beta_expander(GROUP_CATEGORICAL, True):
+                for col in categorical_columns:
+                    section_title(col)
+                    col1, col2 = st.beta_columns(2)
+                    with col1:
+                        count = df[col].value_counts()
+                        count
 
-                st_plot(sns.countplot(df[col]))
+                    with col2:
+                        st_plot(sns.countplot(df[col]))
 
-        with st.beta_expander('Numerical Columns:'):
-            nummeric_columns
-            for col in nummeric_columns:
-                desc = df[col].describe()
-                desc
+        if GROUP_NUMERICAL in selected_groups:
+            with st.beta_expander(GROUP_NUMERICAL, True):
+                for col in nummeric_columns:
+                    section_title(col)
+                    col1, col2 = st.beta_columns(2)
+                    with col1:
+                        desc = df[col].describe()
+                        desc
+                    with col2:
+                        st_plot(sns.distplot(df[col]))
 
-                st_plot(sns.distplot(df[col]))
+        if GROUP_BIVARIATE in selected_groups:
+            with st.beta_expander(GROUP_BIVARIATE, True):
+                for cat_col in categorical_columns:
+                    for num_col in nummeric_columns:
+                        if cat_col != num_col:
+                            col1, col2 = st.beta_columns(2)
+                            with col1:
+                                desc = df[num_col].describe()
+                                desc
+                            
+                            with col2:
+                                st_plot(sns.swarmplot(x=df[cat_col], y=df[num_col]))
 
-        with st.beta_expander('Categorical vs Numerical:'):
-            for cat_col in categorical_columns:
-                for num_col in nummeric_columns:
-                    st_plot(sns.swarmplot(x=df[cat_col], y=df[num_col]))
+                for cat_col in categorical_columns:
+                    for num_col in nummeric_columns:
+                        if cat_col != num_col:
+                            st_plot(sns.boxplot(x=df[cat_col], y=df[num_col]))
 
-            for cat_col in categorical_columns:
-                for num_col in nummeric_columns:
-                    st_plot(sns.boxplot(x=df[cat_col], y=df[num_col]))
-
-        with st.beta_expander('Multivariate Analysis:'):
-            for x in nummeric_columns:
-                for y in nummeric_columns:
-                    if x != y:
-                        for z in categorical_columns:
-                            st_plot(sns.scatterplot(df[x], df[y], df[z]))
+        if GROUP_MULTIVARIATE in selected_groups:
+            with st.beta_expander(GROUP_MULTIVARIATE, True):
+                for x in nummeric_columns:
+                    for y in nummeric_columns:
+                        if x != y:
+                            for z in categorical_columns:
+                                st_plot(sns.scatterplot(df[x], df[y], df[z]))
 
     
 
